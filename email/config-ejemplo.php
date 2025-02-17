8ac075045dc84b5795f89b44c16a841eb3c2e410
JPBM <jpbarba@gmail.com>
2025-02-17 16:19:10 +0100

    Parámetros

--- email/config-ejemplo.php
+++ email/config-ejemplo.php
@@ -4,6 +4,8 @@ ini_set('display_errors', 1);
 
 define('SMTP_SERVER', '');    // Servidor SMTP
 define('SMTP_PORT', 465);     // Puerto
+define('SMTP_AUTH', true);
+define('SMTP_SECURE', 'ssl');
 define('MAIL_USERNAME', '');  // Nombre completo
 define('MAIL_USER', '');      // Nombre de usuario
 define('MAIL_PASS', '');      // Password
