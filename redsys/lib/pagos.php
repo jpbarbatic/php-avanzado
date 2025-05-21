cc4b1c9e49b520b15022584db67393ef21b0c691
JPBM <jpbarba@gmail.com>
2025-05-21 23:43:20 +0200

    Cambio de nombre del fichero

--- redsys/lib/pagos.php
+++ redsys/lib/pagos.php
@@ -44,14 +44,49 @@ function notificacion_pago_cliente($version, $params, $signatureRecibida)
 	}
 }
 
+function actualizar_log_pagos($transaccion)
+{
+    $f='../pagos.csv';
+ 
+    // Si el fichero no existe o si existe y está vacío añadimos la cabecera
+    if(!file_exists($f) or (file_exists($f) and filesize($f)==0)){
+      file_put_contents($f, "Fecha;Hora;Id Pedido;Importe;Núm. tarjeta;Estado\n", FILE_APPEND);
+    }
+    
+    $linea='';
+    $linea.=$transaccion['fecha'].";";
+    $linea.=$transaccion['hora'].";";
+    $linea.=$transaccion['id_pedido'].";";
+    $linea.=$transaccion['importe'].";";
+    $linea.=$transaccion['num_tarjeta'].";";
+    $linea.=$transaccion['resultado_pago']."\n";
+
+    file_put_contents($f, $linea, FILE_APPEND);
+}
+
 function notificacion_pago_web($version, $params, $signatureRecibida){
 	$tpv=new RedsysAPI;
 	$decodec = $tpv->decodeMerchantParameters($params);
 	$firma = $tpv->createMerchantSignatureNotif(DS_SIGNATURE, $params);
 
   if($firma === $signatureRecibida){
-    //Realizamos una acción
-    return $tpv->vars_pay;
+  
+    $params=$tpv->vars_pay;
+    // Obtenemos los campos que van a hacer falta para generar el CVS y el envío del correo
+    $transaccion['importe']=number_format(floatval($params['Ds_Amount'])/100, 2, ',');
+    $transaccion['id_pedido']=$params['Ds_Order'];
+    $transaccion['fecha']=urldecode($params['Ds_Date']);
+    $transaccion['hora']=urldecode($params['Ds_Hour']);
+    $transaccion['num_tarjeta']=$params['Ds_Card_Number'];
+    $transaccion['resultado_pago']=urldecode($params['Ds_Response_Description']);  
+    
+    // Actualizamos fichero de logs de pagos  
+    actualizar_log_pagos($transaccion);
+    
+    // Enviamos un correo 
+    $html=crear_mensaje_plantilla('../vistas/email.html.php', $transaccion);
+    enviar_mail(MAIL_DESTINATARIO, 'Nueva transacción', $html);
+    
 	}else{
 	  return false;
 	}
