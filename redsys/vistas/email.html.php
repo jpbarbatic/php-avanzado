c477017d9c7465348b9572a006f575bc4b4a9ff9
JPBM <jpbarba@gmail.com>
2025-05-21 12:52:51 +0200

    Plantilla de email

---
+++ redsys/vistas/email.html.php
@@ -0,0 +1,5 @@
+<h1>Transacción realizada</h1>
+<p><b>Fecha: </b><?php echo $fecha ?></p>
+<p><b>Hora: </b><?php echo $hora ?></p>
+<p><b>Importe: </b><?php echo $importe ?></p>
+<p><b>Estado: </b><?php echo $resultado_pago ?></p>
